853142147668db9da310e573e1f88b961dd6cea1
Tivere IDORO <105525310+tivereidoro@users.noreply.github.com>
2022-06-23 05:09:45 +0100

    update commit

--- I4G02299876A/blog/models.py
+++ I4G02299876A/blog/models.py
@@ -8,9 +8,11 @@ from django.conf import settings
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    Author = models.ForeignKey()
-    Created_date = models.DateTimeField
-    Published_date = models.DateTimeField
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    Created_date = models.DateTimeField(auto_now_add=True)
+    Published_date = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self().__str__()
